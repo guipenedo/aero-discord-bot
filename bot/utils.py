f35ea89950d72940cf6b3966be443a33ec7ecb3e
guipenedo <nostrumg@gmail.com>
2020-09-29 18:11:36 +0200

    added degree requirement

--- bot/utils.py
+++ bot/utils.py
@@ -1,8 +1,19 @@
+import config
+
+
 def criar_cadeira(cadeira_id):
     # TODO: sacar info da cadeira do fenix, colocar na db, criar channel e role
     return
 
 
 def not_aero(person):
-    # TODO: https://fenixedu.org/dev/api/#get-person percorrer os registos de STUDENT e ver se algum tem o id igual a config.FENIX_DEGREE
-    return False
+    for role in person["roles"]:
+        if role["type"] == "STUDENT":
+            for reg in role["registrations"]:
+                if int(reg["id"]) == int(config.FENIX_DEGREE):
+                    return False
+        elif role["type"] == "ALUMNI":
+            for reg in role["concludedRegistrations"]:
+                if int(reg["id"]) == int(config.FENIX_DEGREE):
+                    return False
+    return True
